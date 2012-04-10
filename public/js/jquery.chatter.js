06b36457ceea2d9d143a600eaeec16efed754611
keith <khowling@salesforce.com>
2012-04-10 11:38:01 +0100

    1

--- public/js/jquery.chatter.js
+++ public/js/jquery.chatter.js
@@ -106,8 +106,37 @@
 				ret = { pic: plugin.feedmembers[pauthor].pic, author: pauthor, ptxt: posttxt[1] };
 			} 
 			
-			var pdate = new Date(Date.parse(itm.createdDate));
-			ret.ctime = $.cuteTime({}, pdate.toString()) + ' : ' + pdate + ' : ' + itm.createdDate;  
+			//var pdate = new Date(Date.parse(itm.createdDate));
+			
+			// https://github.com/csnover/js-iso8601/blob/master/iso8601.js
+			var timestamp, struct, minutesOffset = 0;
+			var origParse = Date.parse, numericKeys = [ 1, 4, 5, 6, 7, 10, 11 ];
+			// 1 YYYY 2 MM 3 DD 4 HH 5 mm 6 ss 7 msec 8 Z 9 ± 10 tzHH 11 tzmm
+			if ((struct = /^(\d{4}|[+\-]\d{6})(?:-(\d{2})(?:-(\d{2}))?)?(?:T(\d{2}):(\d{2})(?::(\d{2})(?:\.(\d{3}))?)?(?:(Z)|([+\-])(\d{2})(?::(\d{2}))?)?)?$/.exec(itm.createdDate))) {
+				// avoid NaN timestamps caused by “undefined” values being passed to Date.UTC
+				for (var i = 0, k; (k = numericKeys[i]); ++i) {
+					struct[k] = +struct[k] || 0;
+				}
+
+				// allow undefined days and months
+				struct[2] = (+struct[2] || 1) - 1;
+				struct[3] = +struct[3] || 1;
+
+				if (struct[8] !== 'Z' && struct[9] !== undefined) {
+					minutesOffset = struct[10] * 60 + struct[11];
+
+					if (struct[9] === '+') {
+						minutesOffset = 0 - minutesOffset;
+					}
+				}
+
+				timestamp = Date.UTC(struct[1], struct[2], struct[3], struct[4], struct[5] + minutesOffset, struct[6], struct[7]);
+			}
+			else {
+				timestamp = origParse ? origParse(itm.createdDate) : NaN;
+			}
+		
+			ret.ctime = $.cuteTime({}, timestamp.toString());  
 			ret.id = itm.id;
 			return ret;
 		}	   
